a07746b11bc3e2eed7a10a7a436d3885b566aafa
Alexey Brodkin <abrodkin@synopsys.com>
2018-10-30 10:44:22 +0300

    LOCAL: gcc: Don't mess with disabling of some components
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -63,16 +63,6 @@ define HOST_GCC_APPLY_PATCHES
 	$(HOST_GCC_APPLY_POWERPC_PATCH)
 endef
 
-HOST_GCC_EXCLUDES = \
-	libjava/* libgo/* \
-	gcc/testsuite/* libstdc++-v3/testsuite/*
-
-define HOST_GCC_FAKE_TESTSUITE
-	mkdir -p $(@D)/libstdc++-v3/testsuite/
-	echo "all:" > $(@D)/libstdc++-v3/testsuite/Makefile.in
-	echo "install:" >> $(@D)/libstdc++-v3/testsuite/Makefile.in
-endef
-
 #
 # Create 'build' directory and configure symlink
 #
