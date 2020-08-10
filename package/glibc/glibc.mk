51413606416ef654ca5977b7e3301621c72e3ccf
Yuriy Kolerov <ykolerov@synopsys.com>
2020-08-10 21:53:03 +0300

    ARCv3: Add support of ARCv3 32-bit and 64-bit processors
    These are new generation of high performance ARC HS5x and HS6x
    cores that implement new ARCv3 ISA binary incompatible with
    previous ARCompact & ARCv2.
    
    We introduce a new menu item for FPU support and based on selection
    enable the canonical BR2_GCC_TARGET_FPU which in turn feeds into
    --with-fpu gcc configure option to gcc.
    
    This --with-fpu is only supported by newer ARC gcc thus the same
    approach is not being done for ARCv2 based HS38/HS48 cores which
    do support FPU. There FPU support is a bit convoluted and is done as
    follows:
    
    * BR2_GCC_TARGET_CPU (e.g BR2_archs38_full => -mcpu=hs38_linux)
    * BR2_TARGET_OPTIMIZATION="-mfpu=fpud_all"
    
    Co-authored-by: Alexey Brodkin <abrodkin@synopsys.com>
    Co-authored-by: Artem Panfilov <artemp@synopsys.com>
    Co-authored-by: Shahab Vahedi <shahab@synopsys.com>
    Co-authored-by: Sergey Matyukevich <sergey.matyukevich@synopsys.com>
    Co-authored-by: Vineet Gupta <vgupta@synopsys.com>
    Co-authored-by: Vladimir Isaev <isaev@synopsys.com>
    Co-authored-by: Yuriy Kolerov <ykolerov@synopsys.com>
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -4,6 +4,11 @@
 #
 ################################################################################
 
+ifeq ($(BR2_arc32)$(BR2_arc64),y)
+GLIBC_VERSION = arc-2025.09
+GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
+BR_NO_CHECK_HASH_FOR += glibc-$(GLIBC_VERSION).tar.gz
+else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
@@ -16,6 +21,7 @@ GLIBC_VERSION = 2.41-70-g1502c248d58cb99a203731707987a4342926e830
 # *NEVER* decide on a version string by looking at the mirror.
 # Then check that the mirror has been synced already (happens once a day.)
 GLIBC_SITE = $(call github,bminor,glibc,$(GLIBC_VERSION))
+endif
 
 GLIBC_LICENSE = GPL-2.0+ (programs), LGPL-2.1+, BSD-3-Clause, MIT (library)
 GLIBC_LICENSE_FILES = COPYING COPYING.LIB LICENSES
