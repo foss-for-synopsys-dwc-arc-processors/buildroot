9865d7e918fc6c6e0dd3e2a0a5842e1fba1f6e1d
Alexey Brodkin <abrodkin@synopsys.com>
2020-08-28 20:59:19 +0300

    ARC64: Use "arc64" branched of GCC/Binutils/glibc for ARC64
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Signed-off-by: Artem Panfilov <panfilov.artyom@gmail.com>
    Signed-off-by: Artem Panfilov <artemp@synopsys.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -4,6 +4,10 @@
 #
 ################################################################################
 
+ifeq ($(BR2_arc64),y)
+GLIBC_VERSION =  arc64
+GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
+else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
@@ -15,6 +19,8 @@ GLIBC_VERSION = 2.34-109-gd64b08d5ba7ffbc9155630f4843cf2e271b1629c
 # *NEVER* decide on a version string by looking at the mirror.
 # Then check that the mirror has been synced already (happens once a day.)
 GLIBC_SITE = $(call github,bminor,glibc,$(GLIBC_VERSION))
+endif
+BR_NO_CHECK_HASH_FOR += glibc-$(GLIBC_VERSION).tar.gz
 
 GLIBC_LICENSE = GPL-2.0+ (programs), LGPL-2.1+, BSD-3-Clause, MIT (library)
 GLIBC_LICENSE_FILES = COPYING COPYING.LIB LICENSES
