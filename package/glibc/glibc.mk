b293154db0760e657345644555c4d137cfc2cdab
Alexey Brodkin <abrodkin@synopsys.com>
2020-08-28 20:59:19 +0300

    ARC64: Switch GCC/Binutils/glibc to arc64 branches
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,6 +7,9 @@
 ifeq ($(BR2_csky),y)
 GLIBC_VERSION = 7630ed2fa60caea98f500e4a7a51b88f9bf1e176
 GLIBC_SITE = $(call github,c-sky,glibc,$(GLIBC_VERSION))
+else ifeq ($(BR2_arc64),y)
+GLIBC_VERSION =  arc64
+GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
