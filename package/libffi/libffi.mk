4409898d3a7d743b0d65b5638c12113161073d28
Vladimir Isaev <isaev@synopsys.com>
2021-02-10 16:30:43 +0300

    ARC64: libffi: Use specific version of libffi
    We do not have ARC64 support in the upstream libffi yet.
    So switch to the our local branch until we have support
    in upstream.
    
    Signed-off-by: Vladimir Isaev <isaev@synopsys.com>

--- package/libffi/libffi.mk
+++ package/libffi/libffi.mk
@@ -4,9 +4,16 @@
 #
 ################################################################################
 
+ifeq ($(BR2_arc),)
 LIBFFI_VERSION = 3.4.2
 LIBFFI_SITE = \
 	https://github.com/libffi/libffi/releases/download/v$(LIBFFI_VERSION)
+else
+LIBFFI_VERSION = arc64
+LIBFFI_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,libffi,$(LIBFFI_VERSION))
+LIBFFI_SOURCE = libffi-$(LIBFFI_VERSION).tar.gz
+BR_NO_CHECK_HASH_FOR += $(LIBFFI_SOURCE)
+endif
 LIBFFI_LICENSE = MIT
 LIBFFI_LICENSE_FILES = LICENSE
 LIBFFI_INSTALL_STAGING = YES
