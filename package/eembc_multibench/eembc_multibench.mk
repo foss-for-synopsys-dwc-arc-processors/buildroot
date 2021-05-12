500bd6d3b5cd93d6ec8cbf318281a230ce6f4761
Vineet Gupta <vgupta@synopsys.com>
2021-05-12 08:40:27 -0700

    multibench: minor fixes

--- package/eembc_multibench/eembc_multibench.mk
+++ package/eembc_multibench/eembc_multibench.mk
@@ -3,7 +3,7 @@
 # MultiBench
 #
 #############################################################
-EEMBC_MULTIBENCH_VERSION = master
+EEMBC_MULTIBENCH_VERSION = origin/master
 EEMBC_MULTIBENCH_SITE = git@gitsnps.internal.synopsys.com:arc_oss/eembc_multibench.git
 EEMBC_MULTIBENCH_SITE_METHOD = git
 define EEMBC_MULTIBENCH_BUILD_CMDS
@@ -13,6 +13,6 @@ define EEMBC_MULTIBENCH_INSTALL_TARGET_CMDS
 	-mkdir -m 0755 $(TARGET_DIR)/multibench/
 	cp -r $(@D)/builds/linux/bin/ $(TARGET_DIR)/multibench/
 	cp -r $(@D)/builds/linux/data/ $(TARGET_DIR)/multibench/
-	cp $(@D)/{multibench.sh,mbench.sh,soak.sh} $(TARGET_DIR)/multibench/bin/
+	cp $(@D)/{multibench.sh,mbench.sh,soak.sh} $(TARGET_DIR)/multibench/
 endef
 $(eval $(generic-package))
