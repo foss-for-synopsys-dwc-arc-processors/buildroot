3b75cd9bc8de67a7b5c5ee3422bb65a6decb38ab
Vineet Gupta <vgupta@synopsys.com>
2021-05-26 12:56:59 -0700

    arc64: gcc: use local gcc git repo
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

---
+++ local.mk
@@ -0,0 +1,9 @@
+GCC_GIT_LOCAL = y
+GCC_OVERRIDE_SRCDIR = /home/vineetg/arc/gnu/arcv3/gcc
+GCC_INITIAL_OVERRIDE_SRCDIR = $(GCC_OVERRIDE_SRCDIR)
+GCC_FINAL_OVERRIDE_SRCDIR = $(GCC_OVERRIDE_SRCDIR)
+HOST_GCC_FINAL_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = --exclude gmp --exclude isl --exclude mpc --exclude mpfr \
+						  --exclude gmp-6.1.0 --exclude isl-0.18 --exclude mpc-1.0.3 --exclude mpfr-3.1.4
+HOST_GCC_INITIAL_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = --exclude gmp --exclude isl --exclude mpc --exclude mpfr \
+						  --exclude gmp-6.1.0 --exclude isl-0.18 --exclude mpc-1.0.3 --exclude mpfr-3.1.4
+
