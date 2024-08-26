4cca959af9d3316cbe793745027db60b45da0d7f
Yuriy Kolerov <ykolerov@synopsys.com>
2024-08-26 13:46:44 +0300

    arc: Use arc-2024.06-release tag for tools
    Affected packages: GCC, Binutils, GDB and glibc. Upstream
    uClibc already contains the latest patches for HS5x.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- package/binutils/binutils.mk
+++ package/binutils/binutils.mk
@@ -9,17 +9,16 @@
 BINUTILS_VERSION = $(call qstrip,$(BR2_BINUTILS_VERSION))
 ifeq ($(BINUTILS_VERSION),)
 ifeq ($(BR2_arc),y)
-BINUTILS_VERSION = arc-2024.06
+BINUTILS_VERSION = arc-2024.06-release
 else
-BINUTILS_VERSION = 2.41
+BINUTILS_VERSION = 2.42
 endif
 endif # BINUTILS_VERSION
 
-ifeq ($(BINUTILS_VERSION),arc-2024.06)
+ifeq ($(BINUTILS_VERSION),arc-2024.06-release)
 BINUTILS_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,binutils-gdb,$(BINUTILS_VERSION))
 BINUTILS_SOURCE = binutils-gdb-$(BINUTILS_VERSION).tar.gz
 BINUTILS_FROM_GIT = y
-BR_NO_CHECK_HASH_FOR += $(BINUTILS_SOURCE)
 endif
 
 BINUTILS_SITE ?= $(BR2_GNU_MIRROR)/binutils
--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -8,11 +8,10 @@ GDB_VERSION = $(call qstrip,$(BR2_GDB_VERSION))
 GDB_SITE = $(BR2_GNU_MIRROR)/gdb
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.xz
 
-ifeq ($(GDB_VERSION),arc-2024.06-gdb)
+ifeq ($(GDB_VERSION),arc-2024.06-release-gdb)
 GDB_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,binutils-gdb,$(GDB_VERSION))
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
 GDB_FROM_GIT = y
-BR_NO_CHECK_HASH_FOR += $(GDB_SOURCE)
 endif
 
 GDB_LICENSE = GPL-2.0+, LGPL-2.0+, GPL-3.0+, LGPL-3.0+
