7882fc726436901f7c6af35909ce2ebe54a5ffb3
Yuriy Kolerov <yuriy.kolerov@globalfoundries.com>
2026-09-10 10:20:31 +0100

    package/gcc: Pass --disable-libatomic to initial GCC
    This will prevent enabling of libatomic when GCC sources are
    overridden.
    
    Signed-off-by: Yuriy Kolerov <yuriy.kolerov@globalfoundries.com>

--- package/gcc/gcc-initial/gcc-initial.mk
+++ package/gcc/gcc-initial/gcc-initial.mk
@@ -40,6 +40,7 @@ HOST_GCC_INITIAL_CONF_OPTS = \
 	--disable-threads \
 	--with-newlib \
 	--disable-largefile \
+	--disable-libatomic \
 	$(call qstrip,$(BR2_EXTRA_GCC_CONFIG_OPTIONS))
 
 HOST_GCC_INITIAL_CONF_ENV = \
